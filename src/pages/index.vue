0fc63fbe86d2d991c655a829d92a9bbc79717684
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2024-05-17 16:05:33 +0300

    feat: add summary section with period select

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,19 @@
+<script setup lang="ts">
+import { viewOptions } from '~/constants'
+
+const view = ref(viewOptions[0])
+</script>
+
 <template>
-  <div>
-    <h1>
-      Finance Tracker
+  <section class="flex items-center justify-between mb-10">
+    <h1 class="text-4xl font-extrabold">
+      Summary
     </h1>
-  </div>
+    <div>
+      <USelectMenu
+        v-model="view"
+        :options="viewOptions"
+      />
+    </div>
+  </section>
 </template>
