1cff5550a26177ecbd41040a80400442deeb3141
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2024-05-29 15:42:18 +0300

    feat(auth): add a login page

---
+++ src/pages/login.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+const success = ref(false)
+
+</script>
+
+<template>
+  <UCard v-if="success">
+    <template #header>
+      Email has been sent
+    </template>
+
+    <template #default>
+      <p class="text-center mb-4">
+        We have sent an email to <strong>rmsil@gmail.com</strong> with a link to sing-in.
+      </p>
+      <p class="text-center">
+        <strong>Important:</strong> The link will expire in 5 minutes.
+      </p>
+    </template>
+  </UCard>
+  <UCard v-else>
+    <template #header>
+      Sign-in to Finance Tracker
+    </template>
+
+    <template #default>
+      <form>
+        <UFormGroup
+          class="mb-4"
+          label="Email"
+          hint="You will receive an email with a confirmation link"
+          name="email"
+          required
+        >
+          <UInput
+            type="email"
+            placeholder="Email"
+            required
+          />
+        </UFormGroup>
+
+        <UButton
+          type="submit"
+          variant="solid"
+          color="black"
+          label="Sign-in"
+          @click="success = true"
+        />
+      </form>
+    </template>
+  </UCard>
+</template>
